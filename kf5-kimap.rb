2b801083222702db472be473aab8aebf4d18cf92
sL1pKn07 <sl1pkn07@gmail.com>
2017-10-28 23:06:38 +0200

    kf5-imap: use stable tarball

--- kf5-kimap.rb
+++ kf5-kimap.rb
@@ -2,7 +2,9 @@ require "formula"
 
 class Kf5Kimap < Formula
   desc "Job-based API for interacting with IMAP servers"
-  homepage "http://www.kde.org/"
+  homepage "https://community.kde.org/KDE_PIM"
+  url "https://download.kde.org/stable/applications/17.08.2/src/kimap-17.08.2.tar.xz"
+  sha256 "ab3a7748d913a4162703a044f9d383cb4f2b395da35b52ebced8419180c7974e"
 
   head "git://anongit.kde.org/kimap.git"
 

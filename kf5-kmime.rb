dd4fa83638ca3f94d65a2e84dc9361de36ebf740
sL1pKn07 <sl1pkn07@gmail.com>
2017-10-28 20:26:47 +0200

    kf5-kmime: use stable tarball

--- kf5-kmime.rb
+++ kf5-kmime.rb
@@ -2,7 +2,9 @@ require "formula"
 
 class Kf5Kmime < Formula
   desc "Library for handling mail messages and newsgroup articles"
-  homepage "http://kde.org/"
+  homepage "https://community.kde.org/KDE_PIM"
+  url "https://download.kde.org/stable/applications/17.08.2/src/kmime-17.08.2.tar.xz"
+  sha256 "6af1916c73278b9255ce28205fe2f94de0f22d1979d785b1062e0343f56723aa"
 
   head "git://anongit.kde.org/kmime.git"
 

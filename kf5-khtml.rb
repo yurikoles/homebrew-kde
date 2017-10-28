50addf1d33ec4d242c6177895fe8ff3ffba012dd
sL1pKn07 <sl1pkn07@gmail.com>
2017-10-29 00:20:38 +0200

    kf5-khtml: fix path during configure (d'oh)

--- kf5-khtml.rb
+++ kf5-khtml.rb
@@ -33,8 +33,8 @@ class Kf5Khtml < Formula
 
   def install
     args = std_cmake_args
-    args << "-DOPENSSL_CRYPTO_LIBRARY=#{CMAKE_PREFIX_PATH}/openssl@1.1/lib/libcrypto.dylib"
-    args << "-DOPENSSL_SSL_LIBRARY=#{CMAKE_PREFIX_PATH}/openssl@1.1/lib/libssl.1.1.dylib"
+    args << "-DOPENSSL_CRYPTO_LIBRARY=#{CMAKE_PREFIX_PATH}/opt/openssl@1.1/lib/libcrypto.dylib"
+    args << "-DOPENSSL_SSL_LIBRARY=#{CMAKE_PREFIX_PATH}/opt/openssl@1.1/lib/libssl.1.1.dylib"
     args << "-DOPENSSL_INCLUDE_DIR=#{CMAKE_PREFIX_PATH}/opt/openssl@1.1/"
 
     mkdir "build" do

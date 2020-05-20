0a28dc38d19e64f35348b25ceb16b858cc3b5eec
Yurii Kolesnykov <root@yurikoles.com>
2020-05-20 23:41:10 +0300

    kf5-sonnet bottle
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/kf5-sonnet.rb
+++ Formula/kf5-sonnet.rb
@@ -4,6 +4,8 @@ class Kf5Sonnet < Formula
   url "https://download.kde.org/stable/frameworks/5.70/sonnet-5.70.0.tar.xz"
   sha256 "1c0cbb3afe7d0e62c6524652626605d04bb9a49074e00d5ef61fd9c05d81082d"
 
+  revision 1
+
   head "git://anongit.kde.org/sonnet.git"
 
   depends_on "cmake" => :build
